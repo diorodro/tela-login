a02fe797b1e86ce6d013b6b177f95be00f4542c8
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-06-08 13:38:54 -0300

    Estilo com Material-UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import FormularioCadastro from './components/FormularioCadastro/';
+import { Container, Typography } from '@material-ui/core';
+import 'fontsource-roboto';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography
+          variant="h3"
+          align="center"
+          component="h1">
+          Formulário de Cadastro
+        </Typography>
+        <FormularioCadastro />
+      </Container>
+    );
+  }
 }
 
 export default App;
+
