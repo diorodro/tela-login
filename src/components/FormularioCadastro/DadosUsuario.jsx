8ad201b57327346312e3870ff813077fd358c82b
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-06-09 10:28:52 -0300

    Renderização condicional com Switch/Case

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -4,9 +4,32 @@ import { TextField, Button } from '@material-ui/core';
 function DadosUsuario() {
     return (
         <form>
-            <TextField id="email" label="email" type="email" variant="outlined" margin="normal" fullWidth/>
-            <TextField id="senha" label="senha" type="password" variant="outlined" margin="normal" fullWidth/>
-            <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
+
+            <TextField
+                id="email"
+                label="email"
+                type="email"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+
+            <TextField
+                id="senha"
+                label="senha"
+                type="password"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+
+            <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+            >
+                Cadastrar
+            </Button>
         </form>
     );
 }
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DadosPessoais from './DadosPessoais';
+import DadosEntrega from './DadosEntrega';
 import DadosUsuario from './DadosUsuario';
+import { Typography } from '@material-ui/core';
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+
+    const [etapaAtual, setEtapaAtual] = useState(1);
+
+    function formularioAtual(etapaAtual) {
+        switch (etapaAtual) {
+            case 0:
+                return <DadosUsuario />;
+            case 1:
+                return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>;
+            case 2:
+                return <DadosEntrega/>;
+            default:
+               return <Typography>ERRO!</Typography>
+        }
+    }
+
     return (
         <>
-            <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-            <DadosUsuario/>
+            {formularioAtual(etapaAtual)}
         </>
     );
 };
-
 
 export default FormularioCadastro;
