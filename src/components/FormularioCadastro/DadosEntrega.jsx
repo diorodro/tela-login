fda354edb6d7729956ce135297be699e01e80bb8
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-06-09 12:07:56 -0300

    Adicionando Stepper e finalização do form

--- src/components/FormularioCadastro/DadosEntrega.jsx
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
 
-function DadosEntrega(params) {
+function DadosEntrega({aoEnviar}) {
+
+    const [cep, setCep] = useState("");
+    const [endereco, setEndereco] = useState("");
+    const [numero, setNumero] = useState(0);
+    const [estado, setEstado] = useState("");
+    const [cidade, setCidade] = useState("");
+
     return (
-        <form>
+        <form onSubmit={event => {
+            event.preventDefault();
+            aoEnviar({ cep, endereco, numero, estado, cidade });
+        }}>
             <TextField
+                value={cep}
+                onChange={event => {
+                    setCep(event.target.value);
+                }}
                 id="cep"
                 label="CEP"
                 type="number"
@@ -13,6 +27,10 @@ function DadosEntrega(params) {
             />
 
             <TextField
+                value={endereco}
+                onChange={event => {
+                    setEndereco(event.target.value);
+                }}
                 id="endereco"
                 label="Endereço"
                 type="text"
@@ -22,6 +40,10 @@ function DadosEntrega(params) {
             />
 
             <TextField
+                value={numero}
+                onChange={event => {
+                    setNumero(event.target.value);
+                }}
                 id="numero"
                 label="Número"
                 type="number"
@@ -30,6 +52,10 @@ function DadosEntrega(params) {
             />
 
             <TextField
+                value={estado}
+                onChange={event => {
+                    setEstado(event.target.value);
+                }}
                 id="estado"
                 label="Estado"
                 type="text"
@@ -38,6 +64,10 @@ function DadosEntrega(params) {
             />
 
             <TextField
+                value={cidade}
+                onChange={event => {
+                    setCidade(event.target.value);
+                }}
                 id="cidade"
                 label="Cidade"
                 type="text"
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,24 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DadosPessoais from './DadosPessoais';
 import DadosEntrega from './DadosEntrega';
 import DadosUsuario from './DadosUsuario';
+import { Typography, Stepper, Step, StepLabel } from '@material-ui/core';
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
 
     const [etapaAtual, setEtapaAtual] = useState(0);
+    const [dadosColetados, setDados] = useState({});
+    useEffect(() => {
+        if (etapaAtual === (formularios.length - 1)) {
+            aoEnviar(dadosColetados);
+        }
+    })
 
     const formularios = [
-        <DadosUsuario aoEnviar={proximo} />,
-        <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
-        <DadosEntrega aoEnviar={aoEnviar} />
+        <DadosUsuario aoEnviar={coletarDados} />,
+        <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+        <DadosEntrega aoEnviar={coletarDados} />,
+        <Typography variant="h5">Obrigado pelo Cadastro!</Typography>
     ];
 
     function proximo() {
         setEtapaAtual(etapaAtual + 1);
     }
 
+    function coletarDados(dados) {
+        setDados({ ...dadosColetados, ...dados });
+        proximo();
+    }
+
     return (
         <>
+            <Stepper activeStep={etapaAtual}>
+                <Step><StepLabel>LOGIN</StepLabel></Step>
+                <Step><StepLabel>PESSOAL</StepLabel></Step>
+                <Step><StepLabel>ENTREGA</StepLabel></Step>
+                <Step><StepLabel>FINALIZAÇÃO</StepLabel></Step>
+            </Stepper>
             {formularios[etapaAtual]}
         </>
     );
